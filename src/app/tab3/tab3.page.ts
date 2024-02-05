00429e2a07ffbbb92f36d1e9d7c8044e10d49d9b
Sebastian Ramirez Vasquez <sebastian.rv.cb.2001@outlook.com>
2024-02-05 15:06:18 -0500

    feat: implmentación de productos en la primera version

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  items: string[] = [];
 
-  constructor() {}
+  ngOnInit() {
+    this.generateItems();
+  }
 
+  private generateItems() {
+    const count = this.items.length + 1;
+    for (let i = 0; i < 15; i++) {
+      this.items.push(`Producto ${count + i}`);
+    }
+  }
+
+  onIonInfinite(ev: InfiniteScrollCustomEvent) {
+    this.generateItems();
+    setTimeout(() => {
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 500);
+  }
 }
